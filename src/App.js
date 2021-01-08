5ef092fcd6c4863a331f06fc1e3de8caae0fe705
denis matijevic <denisone90@gmail.com>
2021-01-08 11:54:09 -0700

    adding a portfolio section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Portfolio />
       <Contact />
     </div>
   );
